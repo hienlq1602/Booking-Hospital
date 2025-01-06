4da59708fd5076087032309167ff87556595cd73
hienlq1602 <136805022+hienlq1602@users.noreply.github.com>
2025-01-06 12:13:51 +0700

    add

--- app/src/main/java/com/ndm/ptit/activity/SignUpActivity.java
+++ app/src/main/java/com/ndm/ptit/activity/SignUpActivity.java
@@ -1,5 +1,6 @@
 package com.ndm.ptit.activity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -21,25 +22,26 @@ import com.ndm.ptit.utils.Utils;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class SignUpActivity extends AppCompatActivity {
 
     private EditText edtPhoneNumber, edtPassword, edtConfirmPassword;
 
-    private TextView tv_login;
+    private TextView tv_login,txtPhoneNumberError,txtToiThieu8,txtConfirmPasswordError;
     private Button btnRegister;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-
         // Find views
         edtPhoneNumber = findViewById(R.id.txtPhoneNumber);
         edtPassword = findViewById(R.id.txtPassword);
         tv_login = findViewById(R.id.tv_login);
+        txtPhoneNumberError = findViewById(R.id.txtPhoneNumberError);
+        txtConfirmPasswordError = findViewById(R.id.txtConfirmPasswordError);
+        txtToiThieu8 = findViewById(R.id.txtToiThieu8);
         edtConfirmPassword = findViewById(R.id.txtConfirmPassword);
         btnRegister = findViewById(R.id.btnRegister);
 
@@ -52,25 +54,24 @@ public class SignUpActivity extends AppCompatActivity {
                 finish();
             }
         });
+
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String phone = edtPhoneNumber.getText().toString();
                 String password = edtPassword.getText().toString();
                 String confirmPassword = edtConfirmPassword.getText().toString();
-
+                checkPhone(phone);
+                checkPassword(password);
+                checkConfirmPassword(confirmPassword,password);
 
                 if (phone.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
                     Toast.makeText(SignUpActivity.this, "Please fill all fields", Toast.LENGTH_SHORT).show();
                     return;
                 }
-
-                if (!password.equals(confirmPassword)) {
-                    Toast.makeText(SignUpActivity.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
-                    return;
+                if(checkPhone(phone) == false || checkPassword(password) == false || checkConfirmPassword(confirmPassword,password) == false){
+                        return;
                 }
-
-
                 SignUpRequest request = new SignUpRequest();
                 request.setEmail("user@gmail.com");
                 request.setType("patient");
@@ -107,7 +108,7 @@ public class SignUpActivity extends AppCompatActivity {
                                 Toast.makeText(SignUpActivity.this, signUpResponse.getMsg(), Toast.LENGTH_SHORT).show();
                             }
                         } else {
-                            Toast.makeText(SignUpActivity.this, "Error: " + response.message(), Toast.LENGTH_SHORT).show();
+                            Toast.makeText(SignUpActivity.this, "Số điện thoại này đã tồn tại " + response.message(), Toast.LENGTH_SHORT).show();
                         }
                     }
 
@@ -118,5 +119,46 @@ public class SignUpActivity extends AppCompatActivity {
                 });
             }
         });
+
     }
+
+    private boolean checkConfirmPassword(String confirmPassword, String password) {
+        if (!password.equals(confirmPassword)) {
+            txtConfirmPasswordError.setVisibility(View.VISIBLE);
+            Toast.makeText(SignUpActivity.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+    private boolean checkPassword(String password) {
+        if(password.length() <8){
+            txtToiThieu8.setVisibility(View.VISIBLE);
+            return false;
+        }
+        else{
+            txtToiThieu8.setVisibility(View.GONE);
+            return true;
+        }
+    }
+
+    private boolean checkPhone(String phone) {
+        if(phone.length()==10){
+            txtPhoneNumberError.setVisibility(View.VISIBLE);
+            return false;
+        }
+        else if(phone.length()>10 ){
+            txtPhoneNumberError.setText("-Sai định sạng số điện thoại");
+            return false;
+        }
+        else if (phone.length()<9 ){
+            txtPhoneNumberError.setText("-Sai định sạng số điện thoại");
+            return false;
+        }
+        else{
+            txtPhoneNumberError.setVisibility(View.GONE);
+            return true;
+        }
+    }
+
 }
--- app/src/main/java/com/ndm/ptit/utils/Utils.java
+++ app/src/main/java/com/ndm/ptit/utils/Utils.java
@@ -3,7 +3,7 @@ package com.ndm.ptit.utils;
 import com.ndm.ptit.enitities.login.LoginRespone;
 
 public class Utils {
-    public static final String BASE_URL = "https://177e-1-53-109-109.ngrok-free.app/";
+    public static final String BASE_URL = "https://c48c-113-22-61-24.ngrok-free.app/";
     public static final String VIDEO_PATH = "https://www.youtube.com/watch?v=W4JA4dbscis&ab_channel=UmbrellaCorporation4";
     public static LoginRespone user;
     public static String service;
